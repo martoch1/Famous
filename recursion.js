ca199de1d0b1bbdf1c6b119a24d4ed93d15d534d
martoch1 <martinsochei@gmail.com>
2023-08-18 04:07:15 -0700

    inital commit

--- recursion.js
+++ recursion.js
@@ -1,20 +1,23 @@
-function goMyCode(word) {
-    if (word.length <= 1) {
+function isPalindrome(word) {
+  // Stop condition: an empty word or a word containing a single character is a palindrome
+  if (word.length <= 1) {
       return true;
-    }
-  
-
-    if (word[0] === word[word.length - 1]) {
-      
-      return goMyCode(word.slice(1, word.length - 1));
-    } else {
-      return false;
-    }
   }
-  
-  console.log(goMyCode(""));
-  console.log(goMyCode("python"));
-  console.log(goMyCode(""));
-  console.log(goMyCode("react")); 
-  console.log(goMyCode("javascript"));
-  
+
+  // Compare the characters at the ends of the word
+  if (word[0] === word[word.length - 1]) {
+      // Recursively test the rest of the word (excluding the compared characters)
+      return isPalindrome(word.slice(1, -1));
+  } else {
+      // Characters are different, not a palindrome
+      return false;
+  }
+}
+
+// Test the function
+console.log(isPalindrome("gag"));    // true
+console.log(isPalindrome("kayak"));  // true
+console.log(isPalindrome("php"));    // true
+console.log(isPalindrome("radar"));  // true
+console.log(isPalindrome("hello"));  // false
+
